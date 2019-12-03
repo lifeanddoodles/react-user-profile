c551d0d62e05ba53a463600dbcb3705e6d520368
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2019-12-02 21:27:20 -0800

    Replace .env variables with better examples

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>{process.env.REACT_APP_TITLE}</h1>
-        <p>{process.env.NODE_ENV}</p>
+        <h1>Welcome to React</h1>
+        <p>
+          You are in <i>{process.env.NODE_ENV}</i> mode.
+        </p>
+        <p>
+          Your 'Secret Code' for this environment would be:{' '}
+          {process.env.REACT_APP_SECRET_CODE}. You can change it in the{' '}
+          <code>.env.{process.env.NODE_ENV}</code> file.
+        </p>
       </div>
     );
   }
