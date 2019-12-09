c0874dc8258ed769a59f8fce5c939f9ba644f493
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2019-12-08 18:03:49 -0800

    Add styles.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.scss';
+import Header from './components/Header';
+import Main from './components/Main';
+import Aside from './components/Aside';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to React</h1>
-        <p>
-          You are in <i>{process.env.NODE_ENV}</i> mode.
-        </p>
-        <p>
-          Your 'Secret Code' for this environment would be:{' '}
-          {process.env.REACT_APP_SECRET_CODE}. You can change it in the{' '}
-          <code>.env.{process.env.NODE_ENV}</code> file.
-        </p>
+      <div id='page' class='grid'>
+        <Header />
+        <Main />
+        <Aside />
+        <Footer />
       </div>
     );
   }
