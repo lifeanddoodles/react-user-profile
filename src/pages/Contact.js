36b5a7163ecb36402b2a0ffdb9b1ee8ed408b1a5
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2019-12-12 20:28:56 -0800

    Add main Page components

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <main>
+      <h1>Contact</h1>
+      <p>You can place a contact form and/or other ways to contact you here.</p>
+    </main>
+  );
+}
---
+++ src/pages/Error404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Error404() {
+  return (
+    <main>
+      <h1>Page Not Found</h1>
+      <p>The page you were looking for does not exist.</p>
+    </main>
+  );
+}
